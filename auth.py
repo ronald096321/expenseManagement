ef41894c434a2916432a18ac5aee7a2d3e7ee8ed
unknown <sohailshaikh2379@gmail.com>
2023-07-02 14:41:42 +0530

    updated file

--- auth.py
+++ auth.py
@@ -1,1 +1,15 @@
 #user authentication
+from os.path import exists;
+from userexist import checkUserExist;
+import csv;
+import json;
+
+
+#Function to add all the expenses to the file
+def authFile() :
+      username = input("Enter the user name : ")
+      pasword = input("Enter the password : ")
+      users = open("users.json","r")
+      x = users.read()
+      userObj = json.loads(x)
+      print("users",len(x))
